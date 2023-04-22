4d97f5871bc66cdab44509c73e87c464d8366d4d
Luoqiu1 <571953991@qq.com>
2023-04-22 15:07:46 +0800

    testâ

---
+++ test/test.py
@@ -0,0 +1,13 @@
+
+def funa(a):
+    return a+10
+
+if __name__ == '__main__':
+    my_string = 'Python is my favorite programming language!'
+    a=type(my_string)
+    help(str.replace)
+    b=funa(1)
+    print(b)
+
+
+
